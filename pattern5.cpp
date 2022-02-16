d4c5c267457b3c53171f9b52b18638fee7fface4
rohit kumar <rohitsach50@gmail.com>
2022-02-16 19:42:38 +0530

    Add files via upload

---
+++ pattern5.cpp
@@ -0,0 +1,22 @@
+# include<iostream>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int x = 1; x <= i; x++)
+        {
+            cout<<i;
+        }
+        
+        cout<<endl;
+    }
+    
+
+
+
+
+
+    }
